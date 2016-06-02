53f1da392521a36dc310ebdafd81d139904dc2e7
rkaliyaperumal <r.kaliyaperumal@lumc.nl>
2016-06-02 11:06:50 +0200

    Process button missed arrow key added

--- js/jquery.buttonLoader.js
+++ js/jquery.buttonLoader.js
@@ -13,7 +13,8 @@
             //disable buttons when loading state
             $('.has-spinner').attr("disabled", "disabled");
             var loderText = "Loading...";
-            $(self).attr('data-btn-text', $(self).text());            
+            $(self).attr('data-btn-text', $(self).html()); 
+            console.log("$(self).text() == " + $(self).html());
             //binding spinner element to button and changing button text
             if(loderText !== undefined && loderText !== "") {
                 $(self).html('<span class="spinner"><i class="fa fa-spinner fa-spin"></i></span>' + loderText);
